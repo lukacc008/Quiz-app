d68271a020107379b9a78914c4b1570199bff4c4
unknown <lukacc008@gmail.com>
2024-08-05 17:22:26 +0200

    Still working on different branch

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -6,7 +6,7 @@ function StartScreen({ onStart }) {
         <p>By pressing the 'I AM READY' button your test will begin.</p>
         <p style={{ color: 'red' }}>Any unanswered questions will be marked as wrong answers.</p>
       <button onClick={onStart}>I AM READY</button>
-    </div> 
+    </div>
   );
 } 
 
